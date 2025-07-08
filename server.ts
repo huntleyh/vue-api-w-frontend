14e511cac4595605790d7ab2958e334b41e428f0
huntleyh <52048389+huntleyh@users.noreply.github.com>
2025-07-08 16:57:54 -0400

    chore: update package.json and server.ts for project setup

--- server.ts
+++ server.ts
@@ -1,10 +1,6 @@
 import express, { Request, Response } from "express";
 import path from 'path';
 import cors from 'cors';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3001;
